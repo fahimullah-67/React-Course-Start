776d3a8a93b946bce2ea5184ad461264e05ee8b1
Fahim Ullah <fahimullah446@gmail.com>
2025-05-13 17:15:10 +0500

    Create react and vite react project

---
+++ counter-app-vite/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React Apps | From Vite</h1>
+      <p>
+        Vite is a build tool that aims to provide a faster and leaner
+        development experience for modern web projects.
+      </p>
+    </div>
+  );
+}
+
+export default App;
